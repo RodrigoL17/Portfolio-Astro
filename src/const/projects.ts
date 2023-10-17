dbfbd7d92413d21f61170a0b70b5c358d0b4203d
Rodrigo Lezama <cordolezama@gmail.com>
2023-10-17 09:29:18 -0300

    project section

---
+++ src/const/projects.ts
@@ -0,0 +1,74 @@
+import type { Projects } from "../types";
+
+export const PROJECTS: Projects[] = [
+  {
+    title: "E-commerce-simulation (Back-End)",
+    description:
+      "Final project 'Back-End Course. CoderHouse' - E-commerce simulation Back-End",
+    imgUrl: "/e-commerce-back-end.png",
+    tecnologies: ["HTML", "CSS", "JavaScript", "NodeJS", "MongoDB","Express", "HandleBars"],
+    gitHubLink: "https://github.com/RodrigoL17/PracticaIntegradora-BackEnd",
+  },
+  {
+    title: "CSS-Gradient-Generator",
+    description:
+      "Personal project - A copy of the original page CSS-Gradient-Generator allows us to obtain the gradients we desire!",
+    imgUrl: "/css-gradient-portada.png",
+    tecnologies: ["HTML", "CSS", "JavaScript", "React", "NextJS"],
+    gitHubLink: "https://github.com/RodrigoL17/css-gradient-generator",
+    urlLink: "https://css-gradient-generator-ten.vercel.app/",
+  },
+  {
+    title: "Calculator",
+    description:
+      "Personal project - Calculator, do all your calculations flawlessly and become a Math whiz!",
+    imgUrl: "/calculadora-portada.png",
+    tecnologies: ["HTML", "CSS", "JavaScript", "React"],
+    gitHubLink: "https://github.com/RodrigoL17/Calculator",
+    urlLink: "https://calculator-one-lime-52.vercel.app/",
+  },
+  {
+    title: "Tic-Tac-Toe",
+    description:
+      "Personal project - Tic-Tac-Toe, a legendary strategy game, incredible to play with a friend... and leave him in ridicule!",
+    imgUrl: "/ta-te-ti-portada.png",
+    tecnologies: ["HTML", "CSS", "JavaScript", "React"],
+    gitHubLink: "https://github.com/RodrigoL17/Tic-Tac-Toe",
+    urlLink: "https://tic-tac-toe-rodrigol17.vercel.app/",
+  },
+  {
+    title: "To-Do-App",
+    description:
+      "Personal project - To-Do app where you can list all you task, and you will never forget them again!",
+    imgUrl: "/to-do-app-portada.png",
+    tecnologies: ["HTML", "CSS", "JavaScript", "React"],
+    gitHubLink: "https://github.com/RodrigoL17/To-Do-List",
+    urlLink: "https://to-do-list-rodrigol17.vercel.app/",
+  },
+  {
+    title: "E-commerce-simulation (Front-End)",
+    description:
+      "Final project 'Front-End Course. CoderHouse' - E-commerce simulation Front-End",
+    imgUrl: "/e-commerce-react-portada.png",
+    tecnologies: ["HTML", "CSS", "JavaScript", "React"],
+    gitHubLink: "https://github.com/RodrigoL17/PracticaIntegradora-FrontEnd",
+    urlLink: "https://e-commerce-front-end.vercel.app/",
+  },
+  {
+    title: "E-commerce-simulation (JS)",
+    description:
+      "Final project 'JavaScript Course. CoderHouse' - E-commerce simulation only with JavaScript",
+    imgUrl: "/e-commerce-js-portada.png",
+    tecnologies: ["HTML", "CSS", "JavaScript"],
+    gitHubLink: "https://github.com/RodrigoL17/E-commerce.JS",
+  },
+  {
+    title: "PsicoNeuroEspiritual",
+    description:
+      "Final project 'Web Developer Course. CoderHouse' - Take a look at our clinics and professionals, schedule a consultation with them!",
+    imgUrl: "/psico-neuro-espiritual-portada.png",
+    tecnologies: ["HTML", "CSS"],
+    gitHubLink: "https://github.com/RodrigoL17/PsicoNeuroEspiritual",
+    urlLink: "https://psico-neuro-espiritual.vercel.app/",
+  },
+];
---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+import type { IconsPaths } from "./components/IconsPath";
+
+export interface Projects {
+    title: string;
+    description: string;
+    imgUrl: string;
+    tecnologies: string[];
+    gitHubLink: string;
+    urlLink?: string;
+}
+    
